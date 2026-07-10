c34f66bb23d7217b2897846926f6e9def70969d9
Your Name <you@example.com>
2026-07-10 14:38:58 +0700

    idk wtf

--- models/gk_controller.py
+++ models/gk_controller.py
@@ -265,6 +265,9 @@ class T4GateKeeperController(models.Model):
         return self.env["t4.gate_keeper.employee"].search(domain)
 
     #### Register 
+
+    def _find_branch_by_code(self, branch_code):
+        return self.env[]
     @endpoint("ControllerRegister")
     def _controller_register (self):
         body = get_body()
