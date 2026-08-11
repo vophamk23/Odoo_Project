5e0abe593bfe525851674b152a03d8fcc0bc4172
XA GIA KHANH <giakhanh2424@gmail.com>
2026-08-11 14:36:05 +0700

    add log wil delete later

--- models/gk_controller.py
+++ models/gk_controller.py
@@ -546,7 +546,31 @@ class T4GateKeeperController(models.Model):
                     finger_templates.append(template)
                 elif biometric.biometric_type == "face":
                     face_templates = template
-            
+
+        if employee:
+            _logger.info("========== EMPLOYEE BIOMETRIC GET ==========")
+            _logger.info("Employee ID: %s", employee.id)
+            _logger.info("Employee emp_id: %s", employee.emp_id)
+            _logger.info("Biometric count: %s", len(employee.biometric_ids))
+
+            for biometric in employee.biometric_ids:
+                _logger.info("---------- BIOMETRIC ----------")
+                _logger.info("Biometric ID: %s", biometric.id)
+                _logger.info("Biometric type: %s", biometric.biometric_type)
+                _logger.info("Binary template exists: %s", bool(biometric.binary_template))
+                _logger.info("Binary template type: %s", type(biometric.binary_template))
+                _logger.info("Binary template value: %s", biometric.binary_template)
+                _logger.info("Char template exists: %s", bool(biometric.char_template))
+                _logger.info("Char template value: %s", biometric.char_template)
+
+                template = (
+                    biometric.binary_template
+                    if biometric.binary_template
+                    else biometric.char_template
+                )
+
+                _logger.info("FINAL TEMPLATE: %s", template)
+                    
 
         return {
             "message": _("Success"),
